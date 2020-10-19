4446001749064a10f5c3ab8235489c30529f5f1c
Couapy <mael@marchand.cloud>
2020-10-19 22:34:13 +0200

    fix(react): add static files like manifest and favicons

--- src/project/settings/base/files.py
+++ src/project/settings/base/files.py
@@ -6,5 +6,8 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'var/static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'var/media/')
 # STATICFILES_DIRS = [ os.path.join(APPS_DIR, 'frontend', 'build', 'static') ]
-STATICFILES_DIRS = [ APPS_DIR + '/frontend/build/static/' ]
+STATICFILES_DIRS = [
+    APPS_DIR + '/frontend/build/',
+    APPS_DIR + '/frontend/build/static/',
+]
 CORS_ORIGIN_ALLOW_ALL = True
